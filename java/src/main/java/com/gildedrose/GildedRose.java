e0da100a0f1128f6c2ec83e9638a8ace721fe987
May-Line Gadonna <mayline.gadonna@gmail.com>
2022-01-11 15:23:53 +0100

    Refactoring of initial code

--- java/src/main/java/com/gildedrose/GildedRose.java
+++ java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.List;
+
 class GildedRose {
     Item[] items;
 
@@ -12,56 +15,71 @@ class GildedRose {
      /!\ Do not change code above this line /!\
      */
 
+    final List<String> CHEESE = Arrays.asList("Aged Brie");
+    final List<String> BACKSTAGE_PASS = Arrays.asList("Backstage passes to a TAFKAL80ETC concert");
+    final List<String> LEGENDARY_ITEM = Arrays.asList("Sulfuras, Hand of Ragnaros");
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+        for (Item item : items) {
+            if (CHEESE.contains(item.name)) {
+                item.sellIn -= 1;
+                updateQualityForCheese(item);
+            } else if (BACKSTAGE_PASS.contains(item.name)) {
+                item.sellIn -= 1;
+                updateQualityForBackstagePass(item);
+            } else if (LEGENDARY_ITEM.contains((item.name))) {
+                // Do nothing.
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+                item.sellIn -= 1;
+                updateQualityForDefaultItem(item);
             }
         }
     }
+
+    private void updateQualityForCheese(Item item) {
+        if (item.sellIn < 0) {
+            item.quality += 2;
+        } else {
+            item.quality += 1;
+        }
+
+        maximumItemQuality(item, 50);
+    }
+
+    private void updateQualityForBackstagePass(Item item) {
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else if (item.sellIn < 5) {
+            item.quality += 3;
+        } else if (item.sellIn < 10) {
+            item.quality += 2;
+        } else {
+            item.quality += 1;
+        }
+
+        maximumItemQuality(item, 50);
+    }
+
+    private void updateQualityForDefaultItem(Item item) {
+        if (item.sellIn >= 0) {
+            item.quality -= 1;
+        } else {
+            item.quality -= 2;
+        }
+
+        minimumItemQuality(item, 0);
+    }
+
+    private void maximumItemQuality(Item item, int max) {
+        if (item.quality > max) {
+            item.quality = max;
+        }
+    }
+
+    private void minimumItemQuality(Item item, int min) {
+        if (item.quality < min) {
+            item.quality = min;
+        }
+    }
+
 }
