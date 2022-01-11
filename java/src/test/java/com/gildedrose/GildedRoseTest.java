4bff39fb03fb57d34b7043f8871d44a41b339cba
May-Line Gadonna <mayline.gadonna@gmail.com>
2022-01-11 15:57:21 +0100

    Replaced Golden Master test by several unit tests

--- java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class GildedRoseTest {
 
@@ -22,49 +22,86 @@ class GildedRoseTest {
         assertEquals(19,foo.sellIn);
         assertEquals(9, foo.quality);
     }
-
+    
     @Test
-    public void goldenMaster() throws IOException {
-        Item[] items = new Item[] {
-            new Item("+5 Dexterity Vest", 10, 20), //
-            new Item("Aged Brie", 2, 0), //
-            new Item("Elixir of the Mongoose", 5, 7), //
-            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-            // this conjured item does not work properly yet
-            new Item("Conjured Mana Cake", 3, 6) };
+    public void backstagePassValueIncreasesButCantBeAbove50() {
+        Item[] items = new Item[]{
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 0),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)
+        };
+
+        Item[] updatedItems = new Item[]{
+            new Item("Backstage passes to a TAFKAL80ETC concert", 14, 1),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 9, 2),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 4, 3),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)
+        };
 
         GildedRose app = new GildedRose(items);
+        app.updateQuality();
 
-        int days = 30;
-        List actualLog = new ArrayList<String>();
-        for (int i = 0; i <= days; i++) {
-            actualLog.add("-------- day " + i + " --------");
-            actualLog.add("name, sellIn, quality");
-            for (Item item : items) {
-                actualLog.add(item.toString());
-            }
-            app.updateQuality();
+        for (int i = 0; i < 3; i++) {
+            assertEquals(updatedItems[0].toString(), app.items[0].toString());
         }
-
-        List expectedLog = new ArrayList<String>();
-        String workingDir = System.getProperty("user.dir");
-        String expectedPath = workingDir + "/../golden-master/expected-output.txt";
-        File expectedFile = new File(expectedPath);
-        FileReader reader = new FileReader(expectedFile);
-        BufferedReader bufferedReader = new BufferedReader(reader);
-        StringBuffer stringBuffer = new StringBuffer();
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            if(! line.isEmpty()) {
-                expectedLog.add(line);
-            }
-        }
-        assertEquals(expectedLog, actualLog);
     }
 
+    @Test
+    public void cheeseValueIncreasesButCantBeAbove50() {
+        Item[] items = new Item[]{
+            new Item("Aged Brie", 10, 10),
+            new Item("Aged Brie", 0, 10),
+            new Item("Aged Brie", 10, 50),
+        };
+
+        Item[] updatedItems = new Item[]{
+            new Item("Aged Brie", 9, 11),
+            new Item("Aged Brie", -1, 12),
+            new Item("Aged Brie", 9, 50),
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        for (int i = 0; i < 3; i++) {
+            assertEquals(updatedItems[0].toString(), app.items[0].toString());
+        }
+    }
+
+    @Test
+    public void legendaryItemsDontChange() {
+        Item[] items = new Item[]{
+            new Item("Sulfuras, Hand of Ragnaros", 1, 80)
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals(1, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
+    }
+
+    @Test
+    public void defaultItemsValueDecreasesButCantBeUnder0() {
+        Item[] items = new Item[]{
+            new Item("An item", 10, 10),
+            new Item("Another item", 0, 10),
+            new Item("Yet another item", 10, 0),
+        };
+
+        Item[] updatedItems = new Item[]{
+            new Item("An item", 9, 9),
+            new Item("Another item", -1, 8),
+            new Item("Yet another item", 9, 0),
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        for (int i = 0; i < 3; i++) {
+            assertEquals(updatedItems[0].toString(), app.items[0].toString());
+        }
+    }
 
 }
